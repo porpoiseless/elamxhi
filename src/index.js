8bdd5373769d9b77d04b6cf936c7c3cd00a86f4c
porpoiseless <darkworldsawake@gmail.com>
2019-06-08 15:52:25 -0700

    Initial rendering of dictionary.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+import { html, render } from 'lit-html';
+import dictionary from './lexicon.json';
+
+const glossaryItem = ({ root, gloss, src }) => html`
+<dt id="${encodeURI(root)}">${root}</dt>
+<dd>
+<p>${gloss}</p>
+<a href="${src}">Source</a>
+</dd>
+`;
+const glossaryList = (items) => html`
+<dl>
+${items.map(glossaryItem)}
+</dl>
+`;
 //
-import * as R from 'ramda';
-window.R = R;
+const renderGlossary = (e) =>
+    render(glossaryList(dictionary.major),
+        document.querySelector('main'));
+// 
+window.addEventListener('load', renderGlossary);
